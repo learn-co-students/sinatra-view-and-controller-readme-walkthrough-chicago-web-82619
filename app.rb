5964ef0a46c69d58b4d2db34676c557c040eeb12
github username <github email address>
2019-09-16 14:06:55 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,3 +1,4 @@
+  
 require_relative 'config/environment'
 
 class App < Sinatra::Base
@@ -6,11 +7,13 @@ class App < Sinatra::Base
   end
 
   post '/reverse' do
+    puts params
+    @rev = params["string"].reverse
     erb :reversed
   end
 
   get '/friends' do
-    # Write your code here!
-
+    @friends = ['Emily Wilding Davison', 'Harriet Tubman', 'Joan of Arc', 'Malala Yousafzai', 'Sojourner Truth']
+    erb :friends
   end
 end
